51006336d14f6ce2cf46342beff10ed563549860
326eunjin <326eunjin@naver.com>
2023-08-05 14:52:32 +0900

    cors

--- config/settings.py
+++ config/settings.py
@@ -145,5 +145,5 @@ DATABASES['default']['ENGINE'] = 'django.contrib.gis.db.backends.postgis'
 GDAL_LIBRARY_PATH = os.environ.get('GDAL_LIBRARY_PATH')
 GEOS_LIBRARY_PATH = os.environ.get('GEOS_LIBRARY_PATH')
 
-CORS_ORIGIN_WHITELIST = ['*']
+CORS_ORIGIN_ALLOW_ALL = True 
 CORS_ALLOW_CREDENTIALS = True
